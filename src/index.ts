8587766ea3e6ce5c4653b43f199cf148d10e69da
teunmos <teun.mos@gmail.com>
2024-04-10 11:01:46 +0200

    added auth0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import proxy from 'express-http-proxy';
+import { auth, requiredScopes } from 'express-oauth2-jwt-bearer';
 import dotenv from "dotenv";
 import cors from 'cors';
 
@@ -9,18 +10,23 @@ const isProduction = process.env.NODE_ENV === "production";
 console.log(`isProduction: ${isProduction}`);
 
 const app: Express = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3004;
 
 const messageApiUrl = process.env.MESSAGE_API_URL || 'http://localhost:3001';
 const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: 'http://localhost/',
+  issuerBaseURL: `https://dev-1qp5piklx43am651.us.auth0.com/`,
+  tokenSigningAlg: 'RS256'
+});
 
 
 console.log(`messageApiUrl: ${messageApiUrl}`);
 console.log(`frontendUrl: ${frontendUrl}`);
-
-// Frontend
 
 
 app.use(cors({
@@ -41,9 +47,22 @@ app.get('/test', (req: Request, res: Response) => {
   res.json({ message: "hello from gateway :)" });
 });
 
-app.use('/api', proxy(messageApiUrl)); // message api
+app.use('/api', checkJwt, (req: Request, res: Response, next: NextFunction) => {
+  if (req.auth) {
+    const userId = req.auth.payload.sub;
+    req.headers['Authorization'] = `Bearer ${req.auth.token}`;
+    req.headers['X-User-Id'] = userId;
 
-app.use(proxy(frontendUrl)); // frontend
+    proxy(messageApiUrl)(req, res, next);
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+});
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  // console.log(`[fr] ${req.method}: ${req.path}`);
+  proxy(frontendUrl)(req, res, next);
+});
 
 
 app.listen(port, () => {
